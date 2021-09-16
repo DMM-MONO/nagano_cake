98c30171aa4bc6b73366140897dc1d9bc6b1eff2
julibou <julibou777@gmail.com>
2021-09-16 12:10:02 +0000

    アイテム、カラム追加

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,25 @@
+class Admin::ItemsController < ApplicationController
+  
+  
+  
+  def show
+  end
+
+  def index
+  end
+
+  def new
+     @item = Item.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+
+end
+
