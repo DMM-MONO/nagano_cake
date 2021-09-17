ae6d0d8ff42ba3e3e3a21c6d760030035c040c2d
osadahiroto <hiroumi879C@gmail.com>
2021-09-17 07:27:28 +0000

    Add Genre

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :items
+  namespace :admin do
+    resources :genres, only: [:edit, :create, :index, :update]
+  end
+  
+  
   
 end
