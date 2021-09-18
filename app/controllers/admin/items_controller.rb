79782abc3ed52aa5a5a827b61c09b289ae86b34e
julibou <julibou777@gmail.com>
2021-09-18 05:18:07 +0000

    商品登録

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ItemsController < ApplicationController
-  
-  
-  
+
+
+
   def show
   end
 
@@ -13,13 +13,21 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.admin_id = admin.id
+    @item.save
+    redirect_to admin_items_path
   end
 
   def edit
   end
-  
+
   def update
   end
+private
 
+  def item_params
+    params.require(:item).permit(:name, :image_id, :price,:is_activ, :genre_id,)
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :items
-  
+   namespace :admin do 
+     resources :items
+  end
 end
