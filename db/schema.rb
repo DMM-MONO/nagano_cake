b61096cb44674f2398c89003930e090321439666
osadahiroto <hiroumi879C@gmail.com>
2021-09-22 05:02:19 +0000

    Add order_details model

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_09_21_071239) do
+ActiveRecord::Schema.define(version: 2021_09_22_045259) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -66,6 +65,16 @@ ActiveRecord::Schema.define(version: 2021_09_21_071239) do
     t.string "image_id"
     t.integer "price"
     t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "item_id"
+    t.integer "price"
+    t.integer "amount"
+    t.integer "making_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
