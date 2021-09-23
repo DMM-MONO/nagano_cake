f97ef9ce4389bb8116c574e29df28a8a6a63acb3
osadahiroto <hiroumi879C@gmail.com>
2021-09-23 05:43:42 +0000

    db

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,7 +3,7 @@ class Public::ItemsController < ApplicationController
     def index
         @item = Item.all
         @items = Item.search(params[:genre_id]).page(params[:page]).per(10)
-        @items = Item.where(is_active: true)
+        # @items = Item.where(is_active: true)
         @genres = Genre.all
         if params[:genre_id]
             @genre = Genre.find(params[:genre_id])
