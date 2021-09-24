18f768b70e333fa3d3d0364086d4a43c4e57034d
julibou <julibou777@gmail.com>
2021-09-24 05:41:20 +0000

    order編集

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrdersController < ApplicationController
+
   def show
-<<<<<<< HEAD
   @order = Order.find(params[:id])
 @order_details = @order.order_details
   end
@@ -13,20 +13,4 @@ class Admin::OrdersController < ApplicationController
 
 
 
-=======
-  @order = Oder.find(params[:id])
-    
-  end
-  
-  def update
-     @oder = Order.find(params[:id])
-    @oder.update(oder_params)
-    
-  end
-  
-    def order_params
-    params.require(:order).permit(:post_code, :address, :name, :postage, :otal_payment, :payment_method, :status,:created_at,:cutomer_id)
-    end
-  
->>>>>>> origin/cake
 end
