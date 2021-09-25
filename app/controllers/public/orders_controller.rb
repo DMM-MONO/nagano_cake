da55b1b3ee11953281db7678d512256f6efb4eda
tuba0809 <tubasa8342849@yahoo.co.jp>
2021-09-25 03:42:05 +0000

    レイアウト修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,8 @@
 class Public::OrdersController < ApplicationController
     
+    def complete
+    end
+    
     def new
         @order=Order.new
         @shipping_address=ShippingAddress.new
@@ -7,27 +10,26 @@ class Public::OrdersController < ApplicationController
     end
     
     def confirm
-        @cart_items=CartItem.where(customer_id:current_customer.id)
-        @order=Order.new(order_params)
-        @address_number=order_params[:address_number]
-        @shipping_address_id=order_params[:shipping_address_id]
-        if @address_number == "0"
-          @order.address = current_customer.address
-          @order.post_code = current_customer.post_code
-          @order.name = current_customer.full_name
-        elsif @address_number == "1"
-          @shipping_address=ShippingAddress.find(@shipping_address_id)
-          @order.address = @shipping_address.post_code
-          @order.post_code = @shipping_address.address
-          @order.name = @shipping_address.name
-        else
-          @shipping_address=ShippingAddress.new
-          @shipping_address.post_code = order_params[:post_code]
-          @shipping_address.address = order_params[:address]
-          @shipping_address.name = order_params[:name]
-        end
-         
-    end 
+      @cart_items=CartItem.where(customer_id:current_customer.id)
+      @order=Order.new(order_params)
+      @address_number=order_params[:address_number]
+      @shipping_address_id=order_params[:shipping_address_id]
+      if @address_number == "0"
+        @order.address = current_customer.address
+        @order.post_code = current_customer.post_code
+        @order.name = current_customer.full_name
+      elsif @address_number == "1"
+        @shipping_address=ShippingAddress.find(@shipping_address_id)
+        @order.address = @shipping_address.post_code
+        @order.post_code = @shipping_address.address
+        @order.name = @shipping_address.name
+      else
+        @shipping_address=ShippingAddress.new
+        @shipping_address.post_code = order_params[:post_code]
+        @shipping_address.address = order_params[:address]
+        @shipping_address.name = order_params[:name]
+      end
+    end
     
     def create
       @address_number=order_params[:address_number]
@@ -35,6 +37,7 @@ class Public::OrdersController < ApplicationController
       @shipping_address_id = order_params[:shipping_address_id]
       if @address_number == "2"
         @order.save
+        #配送先新規登録
         @shipping_address = ShippingAddress.new
         @shipping_address.customer_id = current_customer.id
         @shipping_address.post_code = order_params[:post_code]
@@ -44,32 +47,10 @@ class Public::OrdersController < ApplicationController
       else
         @order.save
       end
-        redirect_to my_page_customer_path
+      redirect_to complete_orders_path
     end
       
-      #elsif @address_number == "1"
-        #@shipping_address=ShippingAddress.find(@shipping_address_id)
-        #@order.address = @shipping_address.post_code
-        #@order.post_code = @shipping_address.address
-        #@order.name = @shipping_address.name
-        #@order.save
-     # else
-       # @order.post_code = order_params[:post_code]
-        #@order.address = order_params[:address]
-        #@order.name = order_params[:name]
-        #@order.save
-        #配送先住所登録
-       # @shipping_address = ShippingAddress.new
-       # @shipping_address.customer_id = current_customer.id
-        #@shipping_address.post_code = order_params[:post_code]
-        #@shipping_address.address = order_params[:address]
-        #@shipping_address.name = order_params[:name]
-        #@shipping_address.save
-      #end
-      #redirect_to my_page_customer_path
-    #end
-    
-    
+   
     private
     
     def order_params
