8e3983202804ee1f7f162f5f308a38e0d22d7c28
julibou <julibou777@gmail.com>
2021-09-25 05:25:54 +0000

    あ

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,7 +1,5 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-
-
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_params)
     redirect_back(fallback_location: root_path)
