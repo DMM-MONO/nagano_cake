74bd68f3cc77eef107f987cce5478339afe67932
osadahiroto <hiroumi879C@gmail.com>
2021-09-26 03:34:41 +0000

    status update

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,7 +6,12 @@ class Admin::OrdersController < ApplicationController
 
   def update
      @order = Order.find(params[:id])
+     @order_detail = @order.order_details
      @order.update(order_params)
+     
+     if params[:order][:status] == "入金確認"
+       @order_detail.update(making_status: 1)
+     end
      redirect_to admin_order_path(@order.id)
   end
 
